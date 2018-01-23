97c5bbd29aac84c861f35c0e4ad717115c02793a
Mateus Souza <mateus_msouza@outlook.com>
2018-01-23 08:52:56 -0200

    Update BSRQST08.py

--- BeautifulSoup/BSRQST08.py
+++ BeautifulSoup/BSRQST08.py
@@ -1,9 +1,9 @@
-import request
+import requests
 
 urlbase = 'http://compras.dados.gov.br'
 cnpj = '07689002000189'
 url = '{0}/contratos/v1/contratos.json?cnpj_contratada={1}'.format(urlbase, cnpj)
 
 request = requests.get(url)
-print(r.json())
+print(request.json())
 
